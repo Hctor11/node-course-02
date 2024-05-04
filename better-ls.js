3501bd30a18d8f2c93a30753f673f0ed795d5304
hector <hectorrrivera1010@gmail.com>
2024-05-03 21:04:28 -0600

    better ls

--- better-ls.js
+++ better-ls.js
@@ -6,13 +6,13 @@ const picocolors = require('picocolors');
 async function ls (folder){
     let files
     try {
-        files = await fs.readdirSync(folder)
+        files = await fs.readdir(folder)
     } catch {
-        console.error(`no se pudo leer el directorio ${folder}`)
+        picocolors.red(console.error(`no se pudo leer el directorio ${folder}`));
         process.exit(1)
     }
 
-    const filePromises = files.map(async (file) => {
+    const filePromises = files.map(async file => {
         const filePath = path.join(folder, file)
         let stats
 
@@ -22,5 +22,59 @@ async function ls (folder){
             console.error(`no se pudo leer el archivo ${filePath}`);
             process.exit(1)
         }
+
+        const isDirectory = stats.isDirectory()
+        const fileType = isDirectory ? 'd' : 'f'
+        const fileSize = stats.size.toString()
+        const fileModified = stats.mtime.toLocaleString()
+
+        return `${picocolors.bgMagenta(fileType)} ${picocolors.blue(file.padEnd(20))} ${picocolors.green(fileSize.padStart(10))} ${picocolors.yellow(fileModified)}`
     })
+
+    const filesInfo = await Promise.all(filePromises)
+    filesInfo.forEach(fileInfo => console.log(fileInfo))
+
 }
+
+ls(folder)
+
+// const fs = require('node:fs/promises')
+// const path = require('node:path')
+// const pc = require('picocolors')
+
+// const folder = process.argv[2] ?? '.'
+
+// async function ls (folder) {
+//   let files
+//   try {
+//     files = await fs.readdir(folder)
+//   } catch {
+//     console.error(pc.red(`❌ No se pudo leer el directorio ${folder}`))
+//     process.exit(1)
+//   }
+
+//   const filesPromises = files.map(async file => {
+//     const filePath = path.join(folder, file)
+//     let stats
+
+//     try {
+//       stats = await fs.stat(filePath) // status - información del archivo
+//     } catch {
+//       console.error(`No se pudo leer el archivo ${filePath}`)
+//       process.exit(1)
+//     }
+
+//     const isDirectory = stats.isDirectory()
+//     const fileType = isDirectory ? 'd' : 'f'
+//     const fileSize = stats.size.toString()
+//     const fileModified = stats.mtime.toLocaleString()
+
+//     return `${pc.bgMagenta(fileType)} ${pc.blue(file.padEnd(20))} ${pc.green(fileSize.padStart(10))} ${pc.yellow(fileModified)}`
+//   })
+
+//   const filesInfo = await Promise.all(filesPromises)
+
+//   filesInfo.forEach(fileInfo => console.log(fileInfo))
+// }
+
+// ls(folder)
