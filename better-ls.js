5b9d5561a8b2cb94fb0c024a9ed98d339be5a09a
hector <hectorrrivera1010@gmail.com>
2024-05-03 20:33:30 -0600

    Add better ls

--- better-ls.js
+++ better-ls.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs/promises');
-const folder = process.argv[2] ?? '.'
+const folder = process.argv[2] ?? '.';
+const path = require('node:path'); 
 
 async function ls (folder){
     let files
@@ -7,5 +8,18 @@ async function ls (folder){
         files = await fs.readdirSync(folder)
     } catch {
         console.error(`no se pudo leer el directorio ${folder}`)
+        process.exit(1)
     }
+
+    const filePromises = files.map(async (file) => {
+        const filePath = path.join(folder, file)
+        let stats
+
+        try{
+            stats = await fs.stat(filePath)
+        }catch{
+            console.error(`no se pudo leer el archivo ${filePath}`);
+            process.exit(1)
+        }
+    })
 }
